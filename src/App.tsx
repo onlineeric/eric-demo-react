28fd6ab5a07ee1a68bbe6a8491467ac612c67acb
Eric Cheng <online.eric@gmail.com>
2024-02-19 17:49:52 +1300

    install package

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
 import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
 import Dashboard from './dashboard/Dashboard';
 
-function Copyright() {
-	return (
-		<Typography variant="body2" color="text.secondary" align="center">
-			{'Copyright © '}
-			<Link color="inherit" href="https://mui.com/">
-				Your Website
-			</Link>{' '}
-			{new Date().getFullYear()}.
-		</Typography>
-	);
-}
 
 export default function App() {
 	return <Dashboard />;
