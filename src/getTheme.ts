3c5888661bd73b57d05a5d61d24ebefe8f33e066
Eric Cheng <online.eric@gmail.com>
2024-02-20 21:49:08 +1300

    update header

--- src/getTheme.ts
+++ src/getTheme.ts
@@ -2,21 +2,21 @@ import { createTheme } from '@mui/material/styles';
 import { red, grey } from '@mui/material/colors';
 
 const getTheme = (mode: 'light' | 'dark') => createTheme({
-  palette: {
-    mode: mode,
-    primary: {
-      main: '#556cd6',
-    },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: mode === 'light' ? grey[100] : grey[800],
-    },
-  },
+	palette: {
+		mode: mode,
+		primary: {
+			main: '#556cd6',
+		},
+		secondary: {
+			main: '#19857b',
+		},
+		error: {
+			main: red.A400,
+		},
+		background: {
+			default: mode === 'light' ? grey[100] : grey[800],
+		},
+	},
 });
 
 export default getTheme;
