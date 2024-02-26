7c4a1dc2692f25072f925ba50cde90f3565914f8
Eric Cheng <online.eric@gmail.com>
2024-02-26 19:08:04 +1300

    separate AuthPage and Dashboard, create Routes

--- src/common/AuthPage.tsx
+++ src/common/AuthPage.tsx
@@ -1,12 +1,57 @@
-import React from 'react';
-import { Navigate, useRoutes } from 'react-router-dom';
+import * as React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import { useTheme } from '@emotion/react';
+import Copyright from '../common/Copyright';
+import AppBar from '../common/AppBar';
+import SideDrawer from '../common/SideDrawer';
 import Dashboard from '../dashboard/Dashboard';
+import { Navigate, useLocation, useRoutes } from 'react-router-dom';
+
+const drawerWidth: number = 240;
 
 export default function AuthPage() {
-	const element = useRoutes([
-		{ path: '/', element: <Dashboard /> },
-		{ path: '*', element: <Navigate to="/" replace /> },
-	]);
+	const [open, setOpen] = React.useState(true);
+	const toggleDrawer = () => {
+		setOpen(!open);
+	};
+	const defaultTheme = useTheme();
 
-	return element;
+	const routes = [
+		{ path: '/home', element: <Dashboard />, title: 'Dashboard Home' },
+		{ path: '/status', element: <div>status page</div>, title: 'Status' },
+		{ path: '*', element: <Navigate to="/home" replace /> },
+	];
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars -- to remove title from routes
+	const routesObjects = useRoutes(routes.map(({ title, ...route }) => route));
+
+	const currentPath = useLocation().pathname;
+	const routeTitles = Object.fromEntries(routes.map(({ path, title }) => [path, title]));
+
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<Box sx={{ display: 'flex' }}>
+				<AppBar open={open} drawerwidth={drawerWidth} toggleDrawer={toggleDrawer} title={routeTitles[currentPath]} />
+				<SideDrawer open={open} drawerwidth={drawerWidth} toggleDrawer={toggleDrawer} />
+				<Box
+					component="main"
+					sx={{
+						backgroundColor: (theme) => theme.palette.background.default,
+						flexGrow: 1,
+						height: '100vh',
+						overflow: 'auto',
+					}}
+				>
+					<Toolbar />
+					<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+						{routesObjects}
+						<Copyright sx={{ pt: 4 }} />
+					</Container>
+				</Box>
+			</Box>
+		</ThemeProvider>
+	);
 }
--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,81 +1,45 @@
 import * as React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Chart from './Chart';
 import Deposits from './Deposits';
 import Orders from './Orders';
-import { useTheme } from '@emotion/react';
-import Copyright from '../common/Copyright';
-import AppBar from '../common/AppBar';
-import SideDrawer from '../common/SideDrawer';
-
-const drawerWidth: number = 240;
 
 export default function Dashboard() {
-	const [open, setOpen] = React.useState(true);
-	const toggleDrawer = () => {
-		setOpen(!open);
-	};
-	const defaultTheme = useTheme();
-
 	return (
-		<ThemeProvider theme={defaultTheme}>
-			<Box sx={{ display: 'flex' }}>
-				<AppBar open={open} drawerwidth={drawerWidth} toggleDrawer={toggleDrawer} title={'Dashboard Home'} />
-				<SideDrawer open={open} drawerwidth={drawerWidth} toggleDrawer={toggleDrawer} />
-				<Box
-					component="main"
+		<Grid container spacing={3}>
+			{/* Chart */}
+			<Grid item xs={12} md={8} lg={9}>
+				<Paper
 					sx={{
-						backgroundColor: (theme) => theme.palette.background.default,
-						flexGrow: 1,
-						height: '100vh',
-						overflow: 'auto',
+						p: 2,
+						display: 'flex',
+						flexDirection: 'column',
+						height: 240,
 					}}
 				>
-					<Toolbar />
-					<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-						<Grid container spacing={3}>
-							{/* Chart */}
-							<Grid item xs={12} md={8} lg={9}>
-								<Paper
-									sx={{
-										p: 2,
-										display: 'flex',
-										flexDirection: 'column',
-										height: 240,
-									}}
-								>
-									<Chart />
-								</Paper>
-							</Grid>
-							{/* Recent Deposits */}
-							<Grid item xs={12} md={4} lg={3}>
-								<Paper
-									sx={{
-										p: 2,
-										display: 'flex',
-										flexDirection: 'column',
-										height: 240,
-									}}
-								>
-									<Deposits />
-								</Paper>
-							</Grid>
-							{/* Recent Orders */}
-							<Grid item xs={12}>
-								<Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-									<Orders />
-								</Paper>
-							</Grid>
-						</Grid>
-						<Copyright sx={{ pt: 4 }} />
-					</Container>
-				</Box>
-			</Box>
-		</ThemeProvider>
+					<Chart />
+				</Paper>
+			</Grid>
+			{/* Recent Deposits */}
+			<Grid item xs={12} md={4} lg={3}>
+				<Paper
+					sx={{
+						p: 2,
+						display: 'flex',
+						flexDirection: 'column',
+						height: 240,
+					}}
+				>
+					<Deposits />
+				</Paper>
+			</Grid>
+			{/* Recent Orders */}
+			<Grid item xs={12}>
+				<Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+					<Orders />
+				</Paper>
+			</Grid>
+		</Grid>
 	);
 }
