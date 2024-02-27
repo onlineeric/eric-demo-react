a704006cebf825148e667a0a54264e9592b522cc
Eric Cheng <online.eric@gmail.com>
2024-02-27 23:05:12 +1300

    created Orders page

--- src/common/routes.tsx
+++ src/common/routes.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Dashboard from '../dashboard/Dashboard';
+import Orders from '../orders/Orders';
 import { Navigate } from 'react-router-dom';
 
 export const routes = [
 	{ path: '/home', element: <Dashboard />, title: 'Dashboard Home' },
+	{ path: '/orders', element: <Orders />, title: 'Orders' },
 	{ path: '/status', element: <div>status page</div>, title: 'Status' },
 	{ path: '*', element: <Navigate to="/home" replace /> },
 ];
