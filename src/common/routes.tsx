e5cdc321dc9823f8f5b2af8fdaf6b0ae35e12f9a
Eric Cheng <online.eric@gmail.com>
2024-03-04 00:19:06 +1300

    logout with call login page

--- src/common/routes.tsx
+++ src/common/routes.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Dashboard from '../dashboard/Dashboard';
 import Orders from '../orders/Orders';
 import { Navigate } from 'react-router-dom';
+import { useAppDispatch } from '../store/hooks';
+import { logOff } from '../store/loginSlice';
+
+// Logoff component
+const Logoff = () => {
+	const dispatch = useAppDispatch();
+	useEffect(() => {
+		console.log('Logoff action');
+		dispatch(logOff());
+	}, []);
+
+	return <div>Logging off...</div>;
+};
 
 export const routes = [
 	{ path: '/home', element: <Dashboard />, title: 'Dashboard Home' },
 	{ path: '/orders', element: <Orders />, title: 'Orders' },
+	{ path: '/login', element: <Logoff />, title: 'Login' },
 	{ path: '/status', element: <div>status page</div>, title: 'Status' },
 	{ path: '*', element: <Navigate to="/home" replace /> },
 ];
